4cd6c3a6df6c92adcc4acc93c584429f63651d34
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-15 14:57:34 -0300

    Create learn more data

---
+++ assets/data/activitiesData.js
@@ -0,0 +1,35 @@
+const activitiesData = [
+    {
+        id: 'activities-1',
+        title: 'KAYAK',
+        image: require('../images/kayak-icon.png')
+    },
+    {
+        id: 'activities-2',
+        title: 'HIKE',
+        image: require('../images/kayak-icon.png')
+    },
+    {
+        id: 'activities-3',
+        title: 'SPA',
+        image: require('../images/spa.png')
+    },
+    {
+        id: 'activities-4',
+        title: 'BIKE',
+        image: require('../images/bike.png')
+    },
+    {
+        id: 'activities-5',
+        title: 'TENNIS',
+        image: require('../images/tennis.png')
+    },
+    {
+        id: 'activities-6',
+        title: 'WATER SKI',
+        image: require('../images/water-ski.png')
+    },
+    
+]
+
+export default activitiesData;
