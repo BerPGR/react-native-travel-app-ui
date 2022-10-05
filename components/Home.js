f0750c54a82c23cae129d6c776196571b51355ec
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-10-04 23:32:01 -0300

    Learn more section

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,8 @@ import colors from '../assets/colors/colors';
 import Feather from 'react-native-vector-icons/Feather';
 import Entypo from 'react-native-vector-icons/Entypo';
 import activitiesData from '../assets/data/activitiesData'
-import discoverData from'../assets/data/discoverData'
+import discoverData from '../assets/data/discoverData'
+import learnMoreData from '../assets/data/learnMoreData'
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import profile from '../assets/images/person.png'
@@ -44,6 +45,19 @@ const Home = ({navigation}) => {
                 <Image source={item.image} style={styles.activityItemImage}/>
                 <Text style={styles.activityItemText}>{item.title}</Text>
             </View>
+        )
+    }
+
+    const renderLearnMoreItem = ({item}) => {
+        return (
+            <ImageBackground source={item.image} 
+                style={[styles.learnMoreItem, {
+                    marginLeft: item.id === 'learnMore-1' ? 20 : 0
+                }]} 
+                imageStyle={styles.learnMoreItemImage}
+            >
+                <Text style={styles.learnMoreItemText}>{item.title}</Text>
+            </ImageBackground>
         )
     }
 
@@ -88,6 +102,20 @@ const Home = ({navigation}) => {
                         <FlatList 
                             data={activitiesData}
                             renderItem={renderActivityItem}
+                            keyExtractor={(item) => item.id}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                        />
+                    </View>
+                </View>
+
+                {/* Learn More */}
+                <View style={styles.learnMoreWrapper}>
+                    <Text style={styles.learnMoreTitle}>Learn More</Text>
+                    <View style={styles.learnMoreItemsWrapper}>
+                        <FlatList 
+                            data={learnMoreData}
+                            renderItem={renderLearnMoreItem}
                             keyExtractor={(item) => item.id}
                             horizontal
                             showsHorizontalScrollIndicator={false}
@@ -192,6 +220,34 @@ const styles = StyleSheet.create({
         fontFamily: 'Lato-Bold',
         fontSize: 14,
         color: colors.grey
+    },
+    learnMoreWrapper: {
+        marginTop: 10
+    },
+    learnMoreTitle: {
+        marginHorizontal: 20,
+        fontFamily: 'Lato-Bold',
+        fontSize: 24,
+        color: colors.black
+    },
+    learnMoreItemsWrapper: {
+        paddingVertical: 20
+    },
+    learnMoreItem: {
+        width: 170,
+        height: 180,
+        justifyContent: 'flex-end',
+        marginRight: 20
+    },
+    learnMoreItemImage: {
+        borderRadius: 20,
+    },
+    learnMoreItemText: {
+        fontFamily: 'Lato-Bold',
+        fontSize: 18,
+        color: colors.white,
+        marginHorizontal: 10,
+        marginVertical: 20
     },
 })
 
